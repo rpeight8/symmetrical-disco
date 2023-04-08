cbaabbd6f7c427e7f26de7994bf5509b1647df0f
RPE8 <vitali.shuman96@gmail.com>
2023-04-08 19:06:19 +0300

    Add NavigationToolbar to the page and make page consume full screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import NavigationToolbar from "@/components/NavigationToolbar/NavigationToolbar";
+import NavigationToolbarOpenCloseButton from "@/components/NavigationToolbar/NavigationToolbarOpenCloseButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <NavigationToolbar />;
 }
