308f2d66bc60ee4c9efe418d075b54fc5576174e
RPE8 <vitali.shuman96@gmail.com>
2023-04-12 20:16:03 +0300

    Add SignIn and Error pages

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,14 @@
+import SignInContent from "@/components/SignInContent";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div>
+      <SignInContent />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/SignInContent.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import GoogleSignInButton from "@/components/ui/GoogleSignInButton";
+
+interface SignInContentProps {}
+
+const SignInContent: FC<SignInContentProps> = ({}) => {
+  return <GoogleSignInButton />;
+};
+
+export default SignInContent;
