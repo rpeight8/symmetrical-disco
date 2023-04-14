c753a04b0f256f0f4b98068df55b698a45b81fba
RPE8 <vitali.shuman96@gmail.com>
2023-04-14 17:07:36 +0300

    Add SignOutButton component

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,24 @@
+import { FC, forwardRef } from "react";
+import Button from "@/components/ui/Button";
+import { signOut } from "next-auth/react";
+
+interface SignOffButtonProps {}
+
+const SignOffButton = forwardRef<HTMLButtonElement, SignOffButtonProps>(
+  ({}, ref) => {
+    return (
+      <Button
+        onClick={() => {
+          signOut();
+        }}
+        ref={ref}
+      >
+        Sign Out
+      </Button>
+    );
+  }
+);
+
+SignOffButton.displayName = "SignOffButton";
+
+export default SignOffButton;
