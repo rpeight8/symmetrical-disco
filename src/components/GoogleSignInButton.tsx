a0d79eb2131f07d993f9db0f710d0d595808b7c0
RPE8 <vitali.shuman96@gmail.com>
2023-04-14 17:07:55 +0300

    Add GoogleSignInButton component

---
+++ src/components/GoogleSignInButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import "client-only";
+import { FC, useState } from "react";
+import { signIn } from "next-auth/react";
+import Button from "@/components/ui/Button";
+
+interface GoogleSignInButtonProps {}
+
+const GoogleSignInButton: FC<GoogleSignInButtonProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signInHandler = async () => {
+    try {
+      setIsLoading(true);
+      await signIn("google");
+    } catch (error) {
+      debugger;
+      console.log({
+        title: "Error signing in",
+        message: "Please try again later.",
+        type: "error",
+      });
+    }
+  };
+
+  return (
+    <Button onClick={signInHandler} isLoading={isLoading}>
+      Sign In using Google
+    </Button>
+  );
+};
+
+export default GoogleSignInButton;
