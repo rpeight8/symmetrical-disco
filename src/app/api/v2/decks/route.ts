bef3b31ba51f10419d3b6a63ea741b3e4d6ae712
RPE8 <vitali.shuman96@gmail.com>
2023-04-16 15:24:11 +0300

    Add test route for decks using /app

--- src/app/api/v2/decks/route.ts
+++ src/app/api/v2/decks/route.ts
@@ -2,6 +2,23 @@ import db from "@/lib/db";
 import { options } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
+// @ts-ignore
+export async function GET(request, response) {
+  const session = await getServerSession(options);
+  console.log(`V2 Session in route: ${JSON.stringify(session)}`);
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const dbDecks = await db.deck.findMany({
+    where: {
+      authorId: session.user.id,
+    },
+  });
+  console.log(`Found ${dbDecks.length} decks for user ${session.user.id}`);
+  return new Response(JSON.stringify(dbDecks));
+}
+
 export async function POST(request: Request) {
   const session = await getServerSession(options);
   if (!session) {
