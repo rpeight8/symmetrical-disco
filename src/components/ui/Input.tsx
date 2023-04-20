c9a332d9aea5deae3db979e203055e2717509bcc
RPE8 <vitali.shuman96@gmail.com>
2023-04-20 20:14:47 +0300

    Add styles to Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,13 +2,40 @@
 
 import "client-only";
 import { FC, InputHTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+const inputVariants = cva(
+  "w-full h-10 px-3 rounded-md border border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent",
+  {
+    variants: {
+      inputDisabled: {
+        true: "bg-primary-disabled-600 cursor-not-allowed",
+        false: "bg-primary-50",
+      },
+      inputSize: {
+        small: "h-8 text-sm",
+        medium: "h-10 text-base",
+        large: "h-12 text-lg",
+      },
+    },
+  }
+);
+
+interface InputProps
+  extends InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
 
 const Input: FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(
-  ({ onChange, value, ...props }, ref) => {
+  ({ onChange, value, inputDisabled, inputSize, ...props }, ref) => {
     return (
-      <input value={value} onChange={onChange} ref={ref} {...props}></input>
+      <input
+        className={twMerge(inputVariants({ inputSize, inputDisabled }))}
+        value={value}
+        onChange={onChange}
+        ref={ref}
+        {...props}
+      ></input>
     );
   }
 );
