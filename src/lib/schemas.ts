fdc3a4bef9c8507d9e89f30a5d19bddc11a1328f
RPE8 <vitali.shuman96@gmail.com>
2023-04-21 17:10:13 +0300

    Add schema.ts for zod schemas and related ts types

---
+++ src/lib/schemas.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const cardSchema = z.object({
+  id: z.string(),
+  deckId: z.string(),
+  question: z.string(),
+  answer: z.string(),
+});
+
+export const cardsSchema = z.array(cardSchema);
+
+export const deckSchema = z.object({
+  id: z.string(),
+  description: z.string().nullable(),
+  name: z.string(),
+  authorId: z.string(),
+  cards: cardsSchema,
+});
+
+export const decksSchema = z.array(deckSchema);
+
+export type Deck = z.infer<typeof deckSchema>[number];
+export type Card = z.infer<typeof cardSchema>;
