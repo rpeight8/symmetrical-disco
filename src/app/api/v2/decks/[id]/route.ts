872ad46e7e079b51a86303b98cf8a7095d4fe151
RPE8 <vitali.shuman96@gmail.com>
2023-04-21 17:14:10 +0300

    Add dynamic api route for decks

---
+++ src/app/api/v2/decks/[id]/route.ts
@@ -0,0 +1,39 @@
+import db from "@/lib/db";
+import { options } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const session = await getServerSession(options);
+  const { id } = params;
+
+  console.log(`/decks[id]: ${id}}`);
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const dbDeck = await db.deck.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      cards: true,
+    },
+  });
+
+  if (!dbDeck) {
+    return new Response("Deck not found", { status: 404 });
+  }
+
+  if (dbDeck.authorId !== session.user.id) {
+    return new Response("Unauthorized to load this deck", { status: 401 });
+  }
+  return new Response(JSON.stringify(dbDeck));
+}
