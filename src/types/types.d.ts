530d77d838f9608d7592a7deba9eaee5eb1b8b1e
RPE8 <vitali.shuman96@gmail.com>
2023-04-25 20:00:20 +0300

    Move types from schemas to types.ts

---
+++ src/types/types.d.ts
@@ -0,0 +1,18 @@
+import {
+  deckSchema,
+  cardSchema,
+  decksSchema,
+  cardsSchema,
+} from "@/lib/schemas";
+import * as z from "zod";
+
+export type Deck = z.infer<typeof deckSchema>;
+export type Decks = z.infer<typeof decksSchema>;
+export type Card = z.infer<typeof cardSchema>;
+export type Cards = z.infer<typeof cardsSchema>;
+
+export type CardForCreation = Omit<Card, "id">;
+export type CardForUpdate = Omit<Card, "deckId">;
+export type CardForDeletion = Pick<Card, "id">;
+
+export type DeckForCreation = Omit<Deck, "id" | "cards" | "authorId">;
