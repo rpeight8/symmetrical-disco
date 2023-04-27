333a81083db674a6439a38280f9ca3f94a273a9d
RPE8 <vitali.shuman96@gmail.com>
2023-04-27 07:26:35 +0300

    Make /decks POST return new deck with cards[]

--- src/app/api/v2/decks/route.ts
+++ src/app/api/v2/decks/route.ts
@@ -58,5 +58,7 @@ export async function POST(request: Request) {
     },
   });
 
-  return new Response(JSON.stringify(createdDeck), { status: 201 });
+  return new Response(JSON.stringify({ ...createdDeck, cards: [] }), {
+    status: 201,
+  });
 }
