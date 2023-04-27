6d853ff17b6d345bf4d44d2f1a39d2c84ade4e2d
RPE8 <vitali.shuman96@gmail.com>
2023-04-27 05:49:37 +0300

    Add title to EditCardAccordion

--- src/components/EditCardAccordion.tsx
+++ src/components/EditCardAccordion.tsx
@@ -11,6 +11,7 @@ import Button from "@/components/ui/Button";
 import CardForm from "@/components/CardForm";
 import { updateCard } from "@/lib/fetch-data";
 import { CardForUpdate } from "@/types/types";
+import Span from "@/components/ui/Span";
 
 interface EditCardAccordionProps extends CardForUpdate {}
 
@@ -23,17 +24,22 @@ const EditCardAccordion: FC<EditCardAccordionProps> = ({
   question = "",
   id,
 }) => {
+  const titleComponent = <Span size="large">{question}</Span>;
   return (
     <Accordion>
       <AccordionItem value="item-1">
-        <AccordionTrigger className="flex flex-row justify-end" asChild>
-          <Button size="medium">Add Card</Button>
+        <AccordionTrigger
+          className="flex flex-row justify-between items-center"
+          titleComponent={titleComponent}
+          asChild
+        >
+          <Button size="medium">Edit Card</Button>
         </AccordionTrigger>
         <AccordionContent>
           <CardForm<CardForUpdate>
             onSubmit={onSubmit}
             card={{ answer, id, question }}
-            actionButtonText="Create Card"
+            actionButtonText="Save Card"
           />
         </AccordionContent>
       </AccordionItem>
