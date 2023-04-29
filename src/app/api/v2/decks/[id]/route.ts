4efb0bd248e3b9967b7e33a4f267b35d8cf3a62b
RPE8 <vitali.shuman96@gmail.com>
2023-04-29 19:45:50 +0300

    Add decks/[id] PUT

--- src/app/api/v2/decks/[id]/route.ts
+++ src/app/api/v2/decks/[id]/route.ts
@@ -36,3 +36,51 @@ export async function GET(
   }
   return new Response(JSON.stringify(dbDeck));
 }
+
+export async function PUT(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const session = await getServerSession(options);
+  const { id } = params;
+
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const dbDeck = await db.deck.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!dbDeck) {
+    return new Response("Deck not found", { status: 404 });
+  }
+
+  if (dbDeck.authorId !== session.user.id) {
+    return new Response("Unauthorized to load this deck", { status: 401 });
+  }
+
+  const { name, description } = (await request.json()) as {
+    name: string;
+    description: string;
+  };
+
+  const updatedDeck = await db.deck.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      description,
+    },
+  });
+
+  console.log(updatedDeck);
+  return new Response(JSON.stringify({ ...updatedDeck, cards: [] }));
+}
