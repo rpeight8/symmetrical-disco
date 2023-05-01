be4ff8b1581beae10bda5e6dc7b56583d8c3bc81
RPE8 <vitali.shuman96@gmail.com>
2023-05-01 17:17:12 +0300

    Add Collapsible component

---
+++ src/components/ui/Collapsible.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import * as RadixCollapsible from "@radix-ui/react-collapsible";
+import { twMerge } from "tailwind-merge";
+
+interface CollapsibleProps {
+  isOpen: boolean;
+  setOpen?: (isOpen: boolean) => void;
+  triggerComponents: JSX.Element[];
+  triggerSiblings?: JSX.Element[];
+  triggerWrapperClassName?: string;
+}
+
+const Collapsible: FC<CollapsibleProps> = ({
+  children,
+  className,
+  triggerWrapperClassName,
+  triggerComponents = [],
+  triggerSiblings = [],
+  isOpen,
+  setOpen,
+}) => {
+  return (
+    <RadixCollapsible.Root
+      open={isOpen}
+      onOpenChange={setOpen}
+      className={twMerge(className)}
+    >
+      <div className={twMerge(triggerWrapperClassName)}>
+        {triggerSiblings}
+        {triggerComponents.map((triggerComponent, index) => {
+          return (
+            <RadixCollapsible.Trigger key={index} asChild>
+              {triggerComponent}
+            </RadixCollapsible.Trigger>
+          );
+        })}
+      </div>
+      <RadixCollapsible.Content>{children}</RadixCollapsible.Content>
+    </RadixCollapsible.Root>
+  );
+};
+
+export default Collapsible;
