5ddf8f9acc430b845c12a8b42aee548246fc78dd
RPE8 <vitali.shuman96@gmail.com>
2023-05-01 17:29:58 +0300

    Replace Accordion based components by Collapsible for deck/[id]

--- src/app/(decks)/deck/[id]/page.tsx
+++ src/app/(decks)/deck/[id]/page.tsx
@@ -1,12 +1,10 @@
 import { headers } from "next/headers";
 import { FC, ReactElement } from "react";
 import { getDeck } from "@/lib/fetch-data";
-import Heading from "@/components/ui/Heading";
-import EditDeckAccordion from "@/components/EditDeckAccordion";
-import DecksList from "@/components/DecksList";
 import Button from "@/components/ui/Button";
 import CardsList from "@/components/CardsList";
-import AddCardAccordion from "@/components/AddCardAccordion";
+import AddCardCollapsible from "@/components/AddCardCollapsible";
+// import AddCardAccordion from "@/components/AddCardAccordion";
 
 interface deckPageProps {
   params: {
@@ -26,18 +24,19 @@ const DeckPage: FC<deckPageProps> = async ({ params }) => {
       {/* <Heading importance="h3" className="max-w-[70%] overflow-hidden">
           {deck.name}
         </Heading> */}
-      <EditDeckAccordion
+      {/* <EditDeckAccordion
         id={deck.id}
         name={deck.name}
         description={deck.description ?? ""}
-      />
+      /> */}
+      <AddCardCollapsible deckId={deck.id}></AddCardCollapsible>
       <div className="flex flex-col mt-5 bg-primary-500 relative z-10">
         <Button size="medium">Practice</Button>
-        <AddCardAccordion
+        {/* <AddCardAccordion
           deckId={deck.id}
           question={""}
           answer={""}
-        ></AddCardAccordion>
+        ></AddCardAccordion> */}
         <CardsList cards={deck.cards}></CardsList>
       </div>
     </section>
