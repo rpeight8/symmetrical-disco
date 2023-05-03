292048a87bee8735e454227921e5f5b75c46f571
RPE8 <vitali.shuman96@gmail.com>
2023-05-03 16:16:14 +0300

    Add DELETE deck/[id]

--- src/app/api/v2/decks/[id]/route.ts
+++ src/app/api/v2/decks/[id]/route.ts
@@ -84,3 +84,41 @@ export async function PUT(
   console.log(updatedDeck);
   return new Response(JSON.stringify({ ...updatedDeck, cards: [] }));
 }
+
+export async function DELETE(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const session = await getServerSession(options);
+  const { id } = params;
+
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const dbDeck = await db.deck.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!dbDeck) {
+    return new Response("Deck not found", { status: 404 });
+  }
+
+  if (dbDeck.authorId !== session.user.id) {
+    return new Response("Unauthorized to load this deck", { status: 401 });
+  }
+
+  const deletedDeck = await db.deck.delete({
+    where: {
+      id,
+    },
+  });
+
+  return new Response(undefined, { status: 204 });
+}
