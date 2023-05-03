63b75e94b753ab0619a1705f2fafa5d3bb2558ff
RPE8 <vitali.shuman96@gmail.com>
2023-05-03 17:46:03 +0300

    Replace CardListItem collabsible by Heading and CardToolbar

--- src/components/CardListItem.tsx
+++ src/components/CardListItem.tsx
@@ -1,16 +1,34 @@
+"use client";
+
+import "client-only";
 import type { Card } from "@/types/types";
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useRef } from "react";
 import { twMerge } from "tailwind-merge";
-import CardItemCollapsible from "@/components/CardItemCollapsible";
+import * as Portal from "@radix-ui/react-portal";
+import Heading from "@/components/ui/Heading";
+import CardToolbar from "@/components/CardToolbar";
 
 interface CardListItemProps extends HTMLAttributes<HTMLLIElement> {
   card: Card;
 }
 
 const CardListItem: FC<CardListItemProps> = ({ card, className }) => {
+  const portalRef = useRef<HTMLDivElement | null>(null);
+
   return (
-    <li className={twMerge("flex justify-between w-full", className)}>
-      <CardItemCollapsible {...card} />
+    <li className={twMerge("flex flex-col w-full", className)}>
+      <div className="w-full flex justify-between">
+        <Heading importance="h3" size="large">
+          {card.question}
+        </Heading>
+        <CardToolbar
+          card={card}
+          Portal={Portal.Root}
+          portalRef={portalRef}
+        ></CardToolbar>
+      </div>
+      <div ref={portalRef}></div>
+      {/* <CardItemCollapsible {...card} /> */}
     </li>
   );
 };
